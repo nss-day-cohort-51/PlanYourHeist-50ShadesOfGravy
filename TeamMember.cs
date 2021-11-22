8be116c120c99ce0bceb2c1f2bff83d154db9948
Matt F <trumpetman072392@gmail.com>
2021-11-22 10:44:59 -0600

    phase-1

---
+++ TeamMember.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace _50ShadesOfGravy
+{
+    public class TeamMember
+    {
+        public string Name { get; set; }
+
+        public int SkillLevel { get; set; }
+
+        public decimal Courage { get; set; }
+
+
+    }
+
+
+}
